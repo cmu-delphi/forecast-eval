3491b0dfa02991178c9e675aff386a5763696edd
Kate Harwood <kateharwood@google.com>
2021-01-28 15:51:51 -0500

    adding in location option for coverage plot

--- dashboard/app.R
+++ dashboard/app.R
@@ -12,7 +12,7 @@ df <- rbind(dfCases, dfDeaths)
 modelChoices = sort(unique(df$forecaster))
 aheadChoices = unique(df$ahead)
 locationChoices = unique(df$geo_value) # TODO maybe make this capitalized
-dateChoices = rev(unique(df$target_end_date))
+dateChoices = rev(sort(as.Date(unique(df$target_end_date))))
 
 ui <- fluidPage(
     titlePanel("Forecast Eval"),
@@ -33,9 +33,17 @@ ui <- fluidPage(
           selected = "COVIDhub-ensemble"
         ),
         radioButtons("ahead", "Ahead",
-                     choices = list("1 week" = aheadChoices[1], "2 weeks" = aheadChoices[2], 
-                                    "3 weeks" = aheadChoices[3], "4 weeks" = aheadChoices[4])),
-        conditionalPanel(condition = "input.scoreType != 'coverage'",
+                     choices = list("1 week" = aheadChoices[1], 
+                                    "2 weeks" = aheadChoices[2], 
+                                    "3 weeks" = aheadChoices[3], 
+                                    "4 weeks" = aheadChoices[4])),
+        
+        conditionalPanel(condition = "input.scoreType == 'coverage'",
+           radioButtons("coverageAggregate", "Aggregate Over",
+                        choices = list("All Locations" = "location", 
+                                       "All Forecast Target Dates" = "date"))
+        ),
+        conditionalPanel(condition = "input.scoreType != 'coverage' || input.coverageAggregate == 'date'",
           selectInput(
             "location",
             "Location",
@@ -44,7 +52,7 @@ ui <- fluidPage(
             selected = "US"
           )
         ),
-        conditionalPanel(condition = "input.scoreType == 'coverage'",
+        conditionalPanel(condition = "input.scoreType == 'coverage' && input.coverageAggregate == 'location'",
           selectInput(
             "date",
             "Date",
@@ -55,22 +63,15 @@ ui <- fluidPage(
       ),
       mainPanel(
         plotOutput(outputId = "summaryPlot"),
-        # dataTableOutput('renderTable'),
+        dataTableOutput('renderTable'),
         textOutput("warningText"),
         h3(tags$div(HTML("<br/>Explanation of scoring methods"))),
-        tags$div(HTML("<b>WIS</b>: The weighted interval score takes into account all the quantile predictions submitted... <br/><br/>")),
-        tags$div(HTML("<b>MAE</b>: <br/><br/>")),
-        tags$div(HTML("<b>Coverage</b>: The coverage plot shows how well a forecaster's confidence intervals performed on a given week, across all locations.
-                      The x-axis is the confidence interval, and the y-xis is the percentage of locations where the ground-truth target variable value fell into that confidence interval.
-                      A perfect forecaster on this measure would follow the bold black line.
-                      <br/>
-                      For example, a forecaster wants the ground-truth value to be within the 50% confidence interval 50% of the time (eg, in 50% of locations).
-                      If the y-value at the 50% confidence interval is above the black line, it means that the ground-truth values fell within the forecaster's 50% CI more than 50% of the time, aka the forecaster's 50% CI was under-confident, or too wide that week.
-                      If the y-values are below the black line, it means that the forecaster's CIs were overconfident that week, or too narrow.
-                      <br/>"))
-        
-        
-                 
+        actionLink("showExplanationWIS", "WIS"),
+        tags$span(HTML("/")),
+        actionLink("showExplanationMAE", "MAE"),
+        tags$span(HTML("/")),
+        actionLink("showExplanationCoverage", "Coverage"),
+        textOutput("scoreExplanation"),
       ),
     ),
 )
@@ -78,17 +79,17 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  summaryPlot = function(scoreDf, target_variable = "cases", score_type = "wis", forecasters = "All",
-                          horizon = 1, loc = "US", date = NULL) {
+  summaryPlot = function(scoreDf, targetVariable = "cases", scoreType = "wis", 
+                         forecasters = "All", horizon = 1, loc = "US", 
+                         coverageAggregateBy = "date", date = NULL) {
     signalFilter = "confirmed_incidence_num"
-    if (target_variable == "deaths") {
+    if (targetVariable == "deaths") {
       signalFilter = "deaths_incidence_num"
     }
     scoreDf <- scoreDf %>% filter(signal == signalFilter) %>%
                            filter(ahead == horizon) %>%
                            filter(forecaster %in% forecasters)
     
-    output$renderTable <- renderDataTable(scoreDf)
     # TODO better way of doing this
     if (nrow(scoreDf) == 0) {
       output$warningText <- renderText("No data for these options.")
@@ -96,8 +97,14 @@ server <- function(input, output, session) {
     } else {
       output$warningText <- renderText("")
     }
-    if (score_type == "coverage") {      
-      scoreDf <- scoreDf %>% filter(target_end_date == date)
+    if (scoreType == "coverage") {
+      # If aggregating over all dates, filter based on the location chosen
+      if (coverageAggregateBy == "date") {
+        scoreDf <- scoreDf %>% filter(geo_value == loc)
+      }
+      if (coverageAggregateBy == "location") {
+        scoreDf <- scoreDf %>% filter(target_end_date == date)
+      }
       if (nrow(scoreDf) == 0) {
         output$warningText <- renderText("No data for these options.")
         return()
@@ -107,14 +114,9 @@ server <- function(input, output, session) {
       
       # Case forecasts only cover 7 quantiles, thus only 3 coverage intervals
       intervals <- c(10,20,30,40,50,60,70,80,90,95,98)
-      if (target_variable == "cases") {
+      if (targetVariable == "cases") {
         intervals <- c(50,80,95)
       }
-      df <- data.frame(Date=as.Date(character()),
-                       File=character(), 
-                       User=character(), 
-                       stringsAsFactors=FALSE) 
-      
       coverageDf <- data.frame(Forecaster=character(), Interval=double(), Score=double(), stringsAsFactors=FALSE)
       forecasters <- unique(scoreDf$forecaster)
       for(i in 1:length(forecasters)) {
@@ -132,22 +134,29 @@ server <- function(input, output, session) {
                    "90" = forecasterDf$cov_90,
                    "95" = forecasterDf$cov_95,
                    "98" = forecasterDf$cov_98)
-          if (target_variable == "cases") {
+          if (targetVariable == "cases") {
             covScore <- switch(toString(intervals[j]),
                     "50" = forecasterDf$cov_50,
                     "80" = forecasterDf$cov_80,
                     "95" = forecasterDf$cov_95)
           }
           forecasterDf <- forecasterDf %>% filter(!is.na(covScore))
-          locations <- unique(forecasterDf$geo_value)
           covScore <- covScore[!is.na(covScore)]
-          covScore <- sum(covScore)/length(locations) * 100
+          if (coverageAggregateBy == "date") {
+            dates <- unique(forecasterDf$target_end_date)
+            covScore <- sum(covScore)/length(dates) * 100
+          }
+          if (coverageAggregateBy == "location") {
+            locations <- unique(forecasterDf$geo_value)
+            covScore <- sum(covScore)/length(locations) * 100
+          }
           coverageDf[nrow(coverageDf) + 1,] = c(forecasters[i], intervals[j], covScore)
         }
       }
       coverageDf$Score <- as.numeric(as.character(coverageDf$Score))
       coverageDf$Interval <- as.numeric(as.character(coverageDf$Interval))
-      output$renderTable <- renderDataTable(coverageDf)
+      
+      output$renderTable <- renderDataTable(scoreDf)
       
       
       ggplot(coverageDf, aes(x = Interval, y = Score)) +
@@ -156,13 +165,13 @@ server <- function(input, output, session) {
         scale_y_continuous(limits=c(0, 100)) +
         scale_x_continuous(limits=c(0, 100)) +
         geom_abline(intercept = 0, slope = 1) +
-        ylab("Coverage")
+        ylab("Coverage Percentage")
     }
     else {
       scoreDf <- scoreDf %>% filter(geo_value == loc)
       axes = aes(x = target_end_date, y = wis)
       ylab = "Weighted Interval Score"
-      if (score_type == "ae") {
+      if (scoreType == "ae") {
         ylab = "Mean Average Error"
         axes = aes(x = target_end_date, y = ae)
       }
@@ -170,15 +179,36 @@ server <- function(input, output, session) {
       ggplot(scoreDf, axes) +
         geom_line(aes(color = forecaster, linetype = forecaster)) +
         geom_point(aes(color = forecaster)) +
-        labs(x = "Date", y = ylab, color = "Forecaster", linetype = "Forecaster")
+        labs(x = "Date", y = ylab, color = "Forecaster", linetype = "Forecaster") +
+        scale_x_date(date_breaks = "months" , date_labels = "%b %Y")
     }
   }
   
-  
-  # output$renderTable <- renderDataTable(scoreDf)
   output$summaryPlot <- renderPlot({
-    summaryPlot(df, input$targetVariable, input$scoreType, input$forecasters, input$ahead,
-                input$location, input$date)
+    summaryPlot(df, input$targetVariable, input$scoreType, input$forecasters, 
+                input$ahead, input$location, input$coverageAggregate, input$date)
+  })
+  
+  observeEvent(input$showExplanationWIS, {
+    output$scoreExplanation <- renderText({
+      "The weighted interval score takes into account all the quantile predictions submitted..."
+    })
+  })
+  observeEvent(input$showExplanationMAE, {
+    output$scoreExplanation <- renderText({
+      "Mean average error is calculated by..."
+    })
+  })
+  observeEvent(input$showExplanationCoverage, {
+    output$scoreExplanation <- renderText({
+      "The coverage plot shows how well a forecaster's confidence intervals performed on a given week, across all locations.
+      The x-axis is the confidence interval, and the y-xis is the percentage of time that the ground-truth target variable value fell into that confidence interval.
+      A perfect forecaster on this measure would follow the bold black line.
+      For example, a forecaster wants the ground-truth value to be within the 50% confidence interval in 50% of locations for the given week (or 50% of weeks for the given location).
+      If the y-value at the 50% confidence interval is above the black line, it means that the ground-truth values fell within the forecaster's 50% CI more than 50% of the time, aka the forecaster's 50% CI was under-confident, or too wide that week.
+      Conversly, if the y-values are below the black line, it means that the forecaster's CIs were overconfident that week, or too narrow.
+      Note: Since forecasters only submit 7 quantiles for case predicitons, there are only 3 confidence intervals for cases."
+    })
   })
   
   # observe({

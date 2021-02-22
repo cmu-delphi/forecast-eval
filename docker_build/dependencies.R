d97dcca555054250508de55fb7c6aae45d415a18
Kate Harwood <kateharwood@google.com>
2021-02-22 14:05:35 -0500

    adding dependencies

--- docker_build/dependencies.R
+++ docker_build/dependencies.R
@@ -1,10 +1,7 @@
 # Tidyverse is installed in the base image
 # Other packages should be installed here
 
-install.packages("assertthat")
-install.packages("optparse")
-install.packages("doParallel")
 install.packages("plotly")
 install.packages("shinyjs")
-
-devtools::install_github("cmu-delphi/covidcast",ref = "evalcast-killcards",subdir = "R-packages/evalcast")
+install.packages("tsibble")
+install.packages("viridis")

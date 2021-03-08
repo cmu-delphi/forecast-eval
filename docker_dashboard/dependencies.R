acd84afb64ca49751fb814e5f80a64f60e1c9c2f
Kate Harwood <kateharwood@google.com>
2021-03-08 15:44:29 -0500

    trying other install source

--- docker_dashboard/dependencies.R
+++ docker_dashboard/dependencies.R
@@ -1,8 +1,8 @@
 # Tidyverse is installed in the base image	
 # Other packages should be installed here	
 	
-install.packages("plotly")	
-install.packages("shinyjs")	
-install.packages("tsibble")	
-install.packages("viridis")
-install.packages("aws.s3")
+install.packages("plotly", dependencies=TRUE, repos='http://cran.rstudio.com/')	
+install.packages("shinyjs", dependencies=TRUE, repos='http://cran.rstudio.com/')	
+install.packages("tsibble", dependencies=TRUE, repos='http://cran.rstudio.com/')	
+install.packages("viridis", dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages("aws.s3", dependencies=TRUE, repos='http://cran.rstudio.com/')

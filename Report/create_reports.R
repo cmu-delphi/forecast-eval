3262e7d95e989d49194619bfd9e971b7d2377153
Jed Grabman <jedg@google.com>
2021-03-25 14:53:37 -0400

    Simplify predictions code

--- Report/create_reports.R
+++ Report/create_reports.R
@@ -21,8 +21,36 @@ prediction_cards_filepath = case_when(
     TRUE~prediction_cards_filename
 )
 
-source("predictions.R")
-create_prediction_cards(prediction_cards_filepath)
+forecasters = c(get_covidhub_forecaster_names(designations = "primary"),
+                "COVIDhub-baseline")
+locations = covidHubUtils::hub_locations
+
+# also includes "us", which is national level data
+state_geos = locations %>%
+                filter(nchar(geo_value) == 2) %>%
+                pull(geo_value)
+
+predictions_cards = get_covidhub_predictions(covidhub_forecaster_name = forecasters,
+                                             signal = c("confirmed_incidence_num",
+                                                        "deaths_incidence_num"),
+                                             geo_values = state_geos)
+predictions_cards = predictions_cards %>%
+    filter(!is.na(predictions_cards$target_end_date)) 
+
+# Only accept forecasts made Monday or earlier
+predictions_cards = predictions_cards %>%
+    filter(target_end_date - (forecast_date + 7 * ahead) >= -2)
+
+# And only a forecaster's last forecast if multiple were made
+predictions_cards = predictions_cards %>% 
+    group_by(forecaster, geo_value, target_end_date, quantile, ahead, signal) %>%
+    filter(forecast_date == max(forecast_date)) %>%
+    ungroup()
+class(predictions_cards) = c("predictions_cards", class(predictions_cards))
+
+saveRDS(predictions_cards,
+        file = "predictions_cards_raw.rds", 
+        compress = "xz")
 
 source("score.R")
 print("Scoring state confirmed incidence...")

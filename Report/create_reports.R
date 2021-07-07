ecb0998eaa582fc00888e040f9440a41af206fde
Kate Harwood <kateharwood@google.com>
2021-07-07 11:20:26 -0400

    include secondary forecasters

--- Report/create_reports.R
+++ Report/create_reports.R
@@ -23,7 +23,7 @@ prediction_cards_filepath = case_when(
     TRUE~prediction_cards_filename
 )
 
-forecasters = c(get_covidhub_forecaster_names(designations = "primary"),
+forecasters = c(get_covidhub_forecaster_names(designations = c("primary", "secondary")),
                 "COVIDhub-baseline")
 locations = covidHubUtils::hub_locations
 

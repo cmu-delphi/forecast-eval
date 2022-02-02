44b1a8a2a4aac07efa05c253062f531770fa645f
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-02-02 17:50:17 -0500

    styler

--- Report/utils.R
+++ Report/utils.R
@@ -4,29 +4,30 @@ check_for_missing_forecasters <- function(predictions_cards, forecasters_list, g
     filter(signal == signal_name) %>%
     distinct(forecaster) %>%
     pull()
-  current_run_forecasters <- predictions_cards %>% 
+  current_run_forecasters <- predictions_cards %>%
     filter(signal == signal_name) %>%
     distinct(forecaster) %>%
     pull()
-  
-  # Find forecasters we asked for that weren't produced. This just prints a 
+
+  # Find forecasters we asked for that weren't produced. This just prints a
   # message because we already know that some forecasters, like CDDEP-ABM,
   # aren't available.
   missing_forecasters <- setdiff(forecasters_list, current_run_forecasters)
   if (length(missing_forecasters) != 0) {
     print(paste(
-      paste(missing_forecasters, collapse = ", "), 
-      "were asked for but not generated")
-    )
+      paste(missing_forecasters, collapse = ", "),
+      "were asked for but not generated"
+    ))
   }
-  
+
   # Find forecasters included in the previous run (based on which ones are
   # included in the relevant score file downloaded from the S3 bucket) that are
   # not in the current run.
   missing_forecasters <- setdiff(previous_run_forecasters, current_run_forecasters)
   assert_that(length(missing_forecasters) == 0,
-         msg = paste(
-           paste(missing_forecasters, collapse = ", "),
-           "were available in the most recent pipeline run but are no longer present")
+    msg = paste(
+      paste(missing_forecasters, collapse = ", "),
+      "were available in the most recent pipeline run but are no longer present"
+    )
   )
 }
